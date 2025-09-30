35ccfdd91e2338264cb75150630c135cc59c50be
Mikail Rizqy <m1kailr0505@gmail.com>
2025-09-30 19:17:35 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -10,6 +10,9 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.squareup.moshi.Moshi
+import retrofit2.converter.moshi.MoshiConverterFactory
+import com.example.lab_week_05.model.ImageData
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
@@ -41,14 +44,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        call.enqueue(object : Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+            override fun onResponse(
+                call: Call<List<ImageData>>,
+                response: Response<List<ImageData>>
+            ) {
                 if (response.isSuccessful) {
-                    apiResponseView.text = response.body()
+                    val images = response.body()
+                    val firstImageUrl = images?.firstOrNull()?.imageUrl ?: "No URL"
+                    apiResponseView.text = getString(R.string.image_placeholder, firstImageUrl)
                 } else {
                     Log.e(
                         MAIN_ACTIVITY, "Failed to get response\n" +
